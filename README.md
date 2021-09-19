7bd11c0a49bef7fcb68bfbb35f31c9567e367ffd
Fran <40085587+Francy-chan@users.noreply.github.com>
2021-09-19 17:58:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Features
+Forces you to use the LEFT HAND as MAIN HAND based on your settings instead of what the server thinks of you!
+
+## License
+Distributed under the GNU LESSER GENERAL PUBLIC LICENSE 2.1. See `LICENSE` for more information.
+
+## Build
+If you want to build this yourself, please clone the repository and execute `gradlew build` in the projects folder. 
+
+Artifacts will be generated at `/build/libs`
---
+++ src/main/java/org/moon/yesimlefty/mixin/PlayerEntityMixin.java
@@ -0,0 +1,28 @@
+package org.moon.yesimlefty.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Arm;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin extends LivingEntity {
+
+    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(at = @At("HEAD"), method = "getMainArm", cancellable = true)
+    public void getMainArm(CallbackInfoReturnable<Arm> cir) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (this.equals(client.player))
+            cir.setReturnValue(client.options.mainArm);
+    }
+
+}
